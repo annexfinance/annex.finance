9bfe34b6697828960c10fa5f1a263dad6fd62840
sharozdev <sharozdev@gmail.com>
2021-09-06 18:47:29 +0500

    TVL value

--- js/custom.js
+++ js/custom.js
@@ -287,7 +287,7 @@ $(function () {
     console.log(TVL);
     const tvl = $(".annex-platform-tvl");
     tvl.html(
-      `TVL ${new Intl.NumberFormat("en-US", {
+      `TVL - ${new Intl.NumberFormat("en-US", {
         style: "currency",
         currency: "USD",
       }).format(TVL)}`
